ddf3730e315ecbd65161bd65c16855bb9868e2cf
jpugmire <jospugg@gmail.com>
2022-05-25 17:49:28 -0400

    bigger line breaks, fix mobile sizing

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -18,21 +18,22 @@ function Experience () {
                     <header><h1>My Experience</h1></header>
                     <div className='info-profile'>My foundation in computer science is built on significant development 
                     experience through internships with eVestment, bolstered by Georgia Tech's Full Stack Development 
-                    Bootcamp. Tap the cards below for more information.</div>
+                    Bootcamp. <br/> <br/>
+                    Tap the cards below for more information.</div>
                 </div>
                 <div className='card-wrapper'>
                     <div className='card' id='ev-card'>
                         <FontAwesomeIcon icon={faSuitcase} size='4x' className='card-item card-icon'/>
-                        <div className='card-item'>Five consecutive internships as a full-stack developer <br/>
-                            Primarily worked with Vue, C#, SQL <br/>
+                        <div className='card-item'>Five consecutive internships as a full-stack developer <br/><br/>
+                            Primarily worked with Vue, C#, SQL <br/><br/>
                             Large exposure to AGILE methodology / project planning
                         </div>
                         <div class='card-overlay'></div>
                     </div>
                     <div className='card' id='gt-card'>
                         <FontAwesomeIcon icon={faGraduationCap} size='4x' className='card-item card-icon'/>
-                        <div className='card-item'>Completed Georgia Tech's Full Stack Bootcamp <br/>
-                            Improved experience and competency with previously learned technologies <br/>
+                        <div className='card-item'>Completed Georgia Tech's Full Stack Bootcamp <br/><br/>
+                            Improved experience and competency with previously learned technologies <br/><br/>
                             Gained experience in new languages and technologies, including noSQL and React,
                             as well as with integrating and deploying new projects.
                         </div>
